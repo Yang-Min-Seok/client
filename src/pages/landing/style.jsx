6507d4a36bde386e6f134bbf9f7ba3dbde5b746d
양민석 <yangminseok@yangminseog-ui-MacBookAir.local>
2023-03-22 22:43:58 +0900

    Landing page framing

---
+++ src/pages/landing/style.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const BodyDiv = styled.div`
+    
+    // Link to teams
+    a {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin: 0 auto;
+        text-decoration: none;
+        font-size: 20px;
+        color: #fff;
+        text-align: center;
+        width:200px;
+        height:50px;
+        background-color: orange;
+        border-radius: 20px;
+    }
+
+`
+export const Logo = styled.div`
+    height:50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+`
+export const Intro = styled.p`
+    margin: 0;
+`
+export const Noti = styled.p`
+    margin: 0;
+    text-decoration: underline;
+    color: orange;
+`
+export const Img = styled.div`
+    height:200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+`
+export const Bar = styled.div`
+    
+`
---
+++ src/styles/global/wrap.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    margin: 0 auto;
+    padding: 0;
+    line-height: 1;
+    list-style: none;
+    max-width: 768px;
+`
+
+export default Wrapper;
