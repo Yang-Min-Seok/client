f8e55baadb449fbff92238ae63a139c03f93c475
양민석 <yangminseok@yangminseog-ui-MacBookAir.local>
2023-04-17 00:20:43 +0900

    Frame 3 css implement (폰트 적용 필요)

--- src/pages/share/body.jsx
+++ src/pages/share/body.jsx
@@ -1,5 +1,6 @@
 import { BodyDiv, Topper, ShareBox, LinkKakao } from "./style";
 import { Link, useLocation } from "react-router-dom";
+import FooterLogoColor from "../../styles/global/footerLogoColor";
 function Body() {
     
     const location = useLocation();
@@ -20,22 +21,20 @@ function Body() {
             <ShareBox>
                 <p>
                     아래의 링크를 공유하여 <br />
-                    모두의 시간표를 <br />
-                    모아주세요 !
+                    모두의 시간표를 모아주세요 !
                 </p>
                 <Link to={`/upload/${teamName}`} state={{url:url, teamId:teamId}}>{`http://localhost:3000/upload/${teamName}`}</Link>
                 
                 <LinkKakao>
                     <div>
-                        링크복사
                     </div>
                     <div>
-                        카카오톡
                     </div>
                 </LinkKakao>
             </ShareBox>
             
             <Link to={`/upload/${teamName}`} state={{url:url, teamId:teamId}}>다음</Link>
+            <FooterLogoColor></FooterLogoColor>
         </BodyDiv>
     )
 }
--- src/pages/share/style.jsx
+++ src/pages/share/style.jsx
@@ -1,17 +1,101 @@
 import styled from "styled-components";
-
+import shareIcon from "../../assets/images/link_copy.png"
+import kakaoIcon from "../../assets/images/kakaotalk.png"
 export const BodyDiv = styled.div`
-    
+    /* link */
+    a:nth-child(3){
+        display: block;
+        width: 155px;
+        height: 45px;
+        text-decoration: none;
+        border-radius: 25px;
+        color: #fff;
+        background-color: #B8B8B8;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 45px;
+        text-align: center;
+        letter-spacing: -0.05em;
+        margin: 0 auto;
+        margin-bottom: 36px;
+    }
 `
 
 export const Topper = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
+    width:10%;
+    margin: 0 auto;
+    margin-top: 15px;
+    margin-bottom: 145px;
+    div {
+        width:10px;
+        height:10px;
+        border-radius: 50%;
+    }
+    div:nth-child(1){
+        background-color: #FF9836;
+    }
+    div:nth-child(2){
+        border: 1px solid #FF9836;
+        box-sizing: border-box;
+    }
 `
 
 export const ShareBox = styled.div`
-    
+    /* intro */
+    p{  
+        margin: 0 auto;
+        display: block;
+        width: 223px;
+        height: 50px;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 25px;
+        text-align: center;
+        letter-spacing: -0.05em;
+        margin-bottom: 19px;
+    }
+    /* share link */
+    a{  
+        display: block;
+        width: 80%;
+        margin: 0 auto;
+        text-align: center;
+        text-decoration: none;
+        color: #FF9836;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 25px;
+        letter-spacing: -0.05em;
+        margin-bottom: 31px;
+    }
 `
 
 export const LinkKakao = styled.div`
-    
+    display: flex;
+    width: 40%;
+    justify-content: space-between;
+    margin: 0 auto;
+    margin-bottom: 115px;
+    div{
+        width: 60px;
+        height: 80px;
+    }
+
+    div:nth-child(1){
+        background-image: url(${shareIcon});
+        background-position: center;
+        background-size: contain;
+        background-repeat: no-repeat;
+    }
+
+    div:nth-child(2){
+        background-image: url(${kakaoIcon});
+        background-position: center;
+        background-size: contain;
+        background-repeat: no-repeat;
+    }
 `
