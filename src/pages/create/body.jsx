ae13e77c47f1b1c7e3dcca105c3ac5b3b0546718
양민석 <yangminseok@yangminseog-ui-MacBookAir.local>
2023-04-17 22:54:55 +0900

    Frame 7 css implement

--- src/pages/create/body.jsx
+++ src/pages/create/body.jsx
@@ -1,36 +1,94 @@
 import { BodyDiv, Intro, TimeBox, OptionBox } from "./style";
 import { Link } from "react-router-dom";
+import FooterLogoBlack from "../../styles/global/footerLogoBlack";
+
+function handleOnClick30min() {
+    const target = document.getElementById("30minLabel");
+    target.style.backgroundColor = "#FF9836";
+    target.style.color = "#fff";
+
+    const except1 = document.getElementById("60minLabel");
+    except1.style.backgroundColor = "#fff";
+    except1.style.color = "#000";
+
+    const except2 = document.getElementById("90minLabel");
+    except2.style.backgroundColor = "#fff";
+    except2.style.color = "#000";
+}
+
+function handleOnClick60min() {
+    const target = document.getElementById("60minLabel");
+    target.style.backgroundColor = "#FF9836";
+    target.style.color = "#fff";
+
+    const except1 = document.getElementById("30minLabel");
+    except1.style.backgroundColor = "#fff";
+    except1.style.color = "#000";
+
+    const except2 = document.getElementById("90minLabel");
+    except2.style.backgroundColor = "#fff";
+    except2.style.color = "#000";
+}
+
+function handleOnClick90min() {
+    const target = document.getElementById("90minLabel");
+    target.style.backgroundColor = "#FF9836";
+    target.style.color = "#fff";
+
+    const except1 = document.getElementById("30minLabel");
+    except1.style.backgroundColor = "#fff";
+    except1.style.color = "#000";
+
+    const except2 = document.getElementById("60minLabel");
+    except2.style.backgroundColor = "#fff";
+    except2.style.color = "#000";
+}
+
+function handleOnSubmit() {
+    // 투표 폼 만들기 눌렀을 때
+
+}
+
 function Body() {
     
     return (
         <BodyDiv>
             
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <Intro>
-                    시간 단위를 설정하면, <br />
-                    투표 폼을 만들 수 <br />
-                    있어요! <br />
-                    만드시겠어요?
+                    <p>
+                        시간 단위를 설정하면,<br />
+                        투표 폼을 만들 수 있어요!<br />
+                        만드시겠어요?
+                    </p>
                 </Intro>
                 
                 <TimeBox>
-                    <input type="checkbox" name="selectedTime" />월요일 9:00 ~ 10:00
+                    <input type="checkbox" name="selectedTime" />월요일 9:00 ~ 10:00<br />
+                    <input type="checkbox" name="selectedTime" />화요일 7:00 ~ 9:00<br />
                 </TimeBox>
 
                 <OptionBox>
-                    <input type="radio" name="timeUnit" />30분
-                    <input type="radio" name="timeUnit" />60분
-                    <input type="radio" name="timeUnit" />90분
+                    <label htmlFor="30min" id="30minLabel" onClick={handleOnClick30min}>30분</label>
+                    <input type="radio" name="timeUnit" id="30min"/>
+                    
+                    <label htmlFor="60min" id="60minLabel" onClick={handleOnClick60min}>60분</label>
+                    <input type="radio" name="timeUnit" id="60min"/>
+                    
+                    <label htmlFor="90min" id="90minLabel" onClick={handleOnClick90min}>90분</label>
+                    <input type="radio" name="timeUnit" id="90min"/>
                 </OptionBox>
 
-                <input type="checkbox" name="duplicate" />복수 선택 허용
+                <input type="checkbox" name="duplicate" id="duplicate"/>
+                <label htmlFor="duplicate" id="duplicateLabel">복수 선택 허용</label>
 
                 <button type="submit">투표 폼 만들기</button>
 
             </form>
 
             <Link to="/">그냥 끝내기</Link>
-
+            
+            <FooterLogoBlack></FooterLogoBlack>
         </BodyDiv>
     )
 }
--- src/pages/create/style.jsx
+++ src/pages/create/style.jsx
@@ -1,15 +1,102 @@
 import styled from "styled-components";
 
 export const BodyDiv = styled.div`
+    margin-top: 74px;
     
-`
+    form {
 
-export const Intro = styled.div`
+    }
     
+    form input:nth-child(4){
+        margin-top: 25px;
+        margin-left: 45px;
+        height: 30px;
+        width: 30px;
+    }
+
+    form #duplicateLabel{
+        display: inline-block;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 25px;
+        letter-spacing: -0.05em;
+        transform: translateY(-7px) translateX(5px);
+    }
+
+    form button:nth-child(6){
+        display: block;
+        margin: 0 auto;
+        margin-top: 60px;
+        border: none;
+        background-color: #FF9836;
+        color: #fff;
+        border-radius: 25px;
+        width: 170px;
+        height: 45px;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 22px;
+        text-align: center;
+        letter-spacing: -0.05em;
+    }
+
+    a {
+        display: block;
+        width: 170px;
+        height: 45px;
+        text-decoration: none;
+        color: #fff;
+        background-color: #888888;
+        border-radius: 25px;
+        margin: 0 auto;
+        margin-top: 16px;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 45px;
+        text-align: center;
+        letter-spacing: -0.05em;
+    }
+`
+export const Intro = styled.div`
+    padding-left: 45px;
+    
+    p{
+        margin: 0;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 25px;
+        letter-spacing: -0.05em;
+    }
 `
 export const TimeBox = styled.div`
-    
+    width: 299px;
+    height: 175px;
+    margin: 0 auto;
+    background-color: #D9D9D9;
+    margin-top: 23px;
+    margin-bottom: 20px;
+    padding: 40px;
+    box-sizing: border-box;
+    overflow: scroll;
 `
 export const OptionBox = styled.div`
-    
+    width: 299px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+
+    input {
+        display: none;
+    }
+
+    label{
+        width: 88px;
+        height: 45px;
+        line-height: 45px;
+        text-align: center;
+        border-radius: 25px;
+        box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.25);
+    }
+
 `
